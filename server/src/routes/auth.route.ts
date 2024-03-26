d568166134db26a7adbb07e83adb1051a4156020
Arya4711 <armlk4711@gmail.com>
2024-03-25 20:58:21 -0400

    Merge leftover changes from dev

--- server/src/routes/auth.route.ts
+++ server/src/routes/auth.route.ts
@@ -1,6 +1,4 @@
 import express, { Request, Response } from "express";
-import { collections } from "../services/database.service";
-import { ObjectId } from "mongodb";
 import User from "../models/user.model";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
@@ -30,7 +28,7 @@ authRouter.post("/login", async (req, res) => {
   try {
     const { username, password } = req.body;
 
-    const user = await collections.users.findOne({ username });
+    const user = await User.findOne({ username });
     if (!user) {
       return res.status(401).json({ error: "Authentication failed" });
     }
@@ -54,10 +52,21 @@ authRouter.post("/updatePassword", async (req, res) => {
   try {
     const { username, password, newPassword } = req.body;
 
-    const user = await collections.users.findOne({ username });
+    const user = await User.findOne({ username });
     if (!user) {
       return res.status(401).json({ error: "Failed to update password" });
     }
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      return res.status(401).json({ error: "Failed to update password" });
+    }
+
+    const hashedNewPassword = await bcrypt.hash(newPassword, 10);
+    user.password = hashedNewPassword;
+    await user.save();
+
+    res.status(200).json({ message: "Password updated successfully" });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Failed to update password" });
