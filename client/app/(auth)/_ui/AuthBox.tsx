3582009c96d643299f7c590adf1558cac51f15e9
Noh Goering <nohgoering@gmail.com>
2024-04-11 23:11:55 -0400

    Add margin to authbox so its centered

--- client/app/(auth)/_ui/AuthBox.tsx
+++ client/app/(auth)/_ui/AuthBox.tsx
@@ -6,7 +6,7 @@ export default function AuthBox({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="py-2 flex justify-between items-center flex-col w-[28rem] h-[28rem] rounded-md bg-grayscale-bg-light dark:bg-grayscale-bg-dark border-accent-border border">
+    <div className="mt-12 py-2 flex justify-between items-center flex-col w-[28rem] h-[28rem] rounded-md bg-grayscale-bg-light dark:bg-grayscale-bg-dark border-accent-border border">
       {children}
     </div>
   );
