662a7398ea02b09d213827457f6a43a44333be65
Arya4711 <armlk4711@gmail.com>
2024-04-12 21:52:57 -0400

    Begin data fetching logic

--- client/app/(auth)/login/LoginBox.tsx
+++ client/app/(auth)/login/LoginBox.tsx
@@ -3,6 +3,7 @@
 
 // Dependencies
 import Link from "next/link";
+import useSWR from "swr";
 
 // Assets
 import ContainerBox from "../_ui/ContainerBox";
@@ -10,17 +11,20 @@ import InputBox from "../_ui/InputBox";
 import InputButton from "../_ui/InputButton";
 
 export default function LoginClient() {
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const useSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     const form = e.target as HTMLFormElement;
     const formData = new FormData(form);
+    const formJson = Object.fromEntries(formData.entries());
+    const fetcher = (url: string) => fetch(url).then((res) => res.json());
+    const { data, error } = useSWR("localhost:5050/auth/login", fetcher);
 
     console.log(Object.fromEntries(formData.entries()));
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={useSubmit}>
       <ContainerBox>
         <div className="text-3xl mt-10 dark:text-grayscale-50">
           Log in to Happy Habits
