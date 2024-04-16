df7364a104851c06e7264ff59140ddbcf360fa2d
Noh Goering <nohgoering@gmail.com>
2024-04-16 15:55:36 -0400

    Switch auth groundwork to reusable component

--- client/app/(auth)/login/page.tsx
+++ client/app/(auth)/login/page.tsx
@@ -1,15 +1,12 @@
-import LoginClient from "./LoginBox";
-import Logo from "../../_ui/Logo";
+import LoginBox from "./LoginBox";
+import AuthBasic from "../_ui/AuthBasic";
 
 export const metadata = { title: "Login", description: "Login page" };
 
 export default function Login() {
   return (
-    <div className="overflow-y-scroll bg-gradient-to-t from-grayscale-300 min-h-screen to-grayscale-50 dark:from-grayscale-950 dark:to-grayscale-600">
-      <Logo route="/" />
-      <div className="flex justify-center items-center">
-        <LoginClient />
-      </div>
-    </div>
+    <AuthBasic>
+      <LoginBox />
+    </AuthBasic>
   );
 }
