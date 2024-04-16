74996f82d0ca99973cd6a0c7f5bd14f702e6c983
Noh Goering <nohgoering@gmail.com>
2024-04-16 15:12:07 -0400

    Add start of auth cookie

--- client/app/(auth)/login/LoginBox.tsx
+++ client/app/(auth)/login/LoginBox.tsx
@@ -1,10 +1,8 @@
 /* eslint-disable react/no-unescaped-entities */
 "use client";
-
 // Dependencies
 import Link from "next/link";
 import login from "../_lib/login";
-import { useState } from "react";
 
 // Assets
 import ContainerBox from "../_ui/ContainerBox";
@@ -12,13 +10,14 @@ import InputBox from "../_ui/InputBox";
 import InputButton from "../_ui/InputButton";
 
 export default function LoginClient() {
-  const [response, setResponse] = useState();
-
-  console.log(response);
-
   async function setCookies(formData: FormData) {
     const response = await login(formData);
-    setResponse(response);
+
+    if (response.status != 200) {
+      console.log("bad");
+    }
+    const data = await response.json();
+    document.cookie = `token=Bearer ${data.token}`;
   }
 
   return (
