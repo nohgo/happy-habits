a4c0ece0a70dd00297936557a38fd286dcb1abd7
Noh Goering <nohgoering@gmail.com>
2024-04-18 13:07:55 -0400

    Fix formatting on register

--- client/app/(auth)/register/_ui/Step.tsx
+++ client/app/(auth)/register/_ui/Step.tsx
@@ -1,5 +1,6 @@
 import InputBox, { IInputBox } from "../../_ui/InputBox";
 import Button from "../../_ui/Button";
+import Link from "next/link";
 
 export interface IStep extends IInputBox {
   action: (formData: FormData) => void;
@@ -15,14 +16,25 @@ export default function Step({
   invalidError,
 }: IStep) {
   return (
-    <form action={action}>
+    <form action={action} className="flex flex-col justify-between items-center flex-grow">
+      <div className="mt-10 text-3xl dark:text-grayscale-50">
+        Register for Happy Habits
+      </div>
       <InputBox
         id={id}
         placeholder={placeholder}
         type={type}
         invalidError={invalidError}
       />
+      <div>
       <Button text={index === 2 ? "Register" : "Continue"} />
+      <Link
+        href="/login"
+        className="block underline transition hover:no-underline dark:text-grayscale-50"
+      >
+        Already have an account? Log in
+      </Link>
+      </div>
     </form>
   );
 }
