fe61f86dac980a80eaa735389b81912d9994bcde
nohgo <nohgoering@gmail.com>
2024-05-28 19:56:03 -0400

    fix errors with habit incrementing

--- server/src/services/habits.service.ts
+++ server/src/services/habits.service.ts
@@ -6,7 +6,7 @@ export async function addHabit(
   username: string,
   name: string,
   description: string,
-  frequency: number
+  frequency: number,
 ) {
   if (!name || !description || !frequency) {
     throw new Error("Missing required fields");
@@ -37,12 +37,12 @@ export async function updateHabit(
   habitId: string,
   name: string,
   description: string,
-  frequency: number
+  frequency: number,
 ) {
   if (!username || !habitId || !name || !description || !frequency) {
     throw new Error("Missing required fields");
   }
-  
+
   const user = await User.findOne({ username });
   if (!user) throw new Error("User not found");
   if (!user.habits.includes(new ObjectId(habitId)))
@@ -56,7 +56,7 @@ export async function updateHabit(
         description,
         frequency,
       },
-    }
+    },
   );
 }
 
@@ -73,7 +73,7 @@ export async function deleteHabit(username: string, habitId: string) {
 
 export async function deleteMultipleHabits(
   username: string,
-  habitIds: Array<string>
+  habitIds: Array<string>,
 ) {
   if (!username || !habitIds) {
     throw new Error("Missing required fields");
@@ -103,8 +103,9 @@ export async function incrementStreak(username: string, habitId: string) {
 
   const user = await User.findOne({ username });
   if (!user) throw new Error("User not found");
-  if (!user.habits.includes(new ObjectId(habitId)))
-    {throw new Error("Habit not found");}
+  if (!user.habits.includes(habitId as unknown as ObjectId)) {
+    throw new Error("Habit not found");
+  }
 
   const habit = await Habit.findOne({ _id: habitId });
   const now = new Date();
@@ -112,10 +113,12 @@ export async function incrementStreak(username: string, habitId: string) {
   if (habit.lastIncrement === null) {
     habit.streak += 1;
     habit.lastIncrement = now;
+    await habit.save();
+    return;
   }
 
   const diffInDays = Math.floor(
-    (now.getTime() - habit.lastIncrement.getTime()) / (1000 * 60 * 60 * 24)
+    (now.getTime() - habit.lastIncrement.getTime()) / (1000 * 60 * 60 * 24),
   );
 
   if (diffInDays >= habit.frequency) {
@@ -152,6 +155,6 @@ export async function resetAllStreaks(username: string) {
   const user = await User.findOne({ username: username });
   await Habit.updateMany(
     { _id: { $in: user.habits } },
-    { $set: { streak: 0 } }
+    { $set: { streak: 0 } },
   );
 }
