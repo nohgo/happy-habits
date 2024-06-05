13c808509a72c95af54e57e40620e1d96438e129
nohgo <nohgoering@gmail.com>
2024-06-04 20:41:18 -0400

    add sorting functionality

---
+++ client/app/dashboard/_lib/comparators.ts
@@ -0,0 +1,41 @@
+import { IHabit } from "../_ui/Habit";
+
+export function nameComparator(a: IHabit, b: IHabit): number {
+  if (a.name < b.name) {
+    return -1;
+  }
+  if (a.name > b.name) {
+    return 1;
+  }
+  return 0;
+}
+
+export function descriptionComparator(a: IHabit, b: IHabit): number {
+  if (a.description < b.description) {
+    return -1;
+  }
+  if (a.name > b.name) {
+    return 1;
+  }
+  return 0;
+}
+
+export function streakComparator(a: IHabit, b: IHabit): number {
+  return a.streak - b.streak;
+}
+
+export function frequencyComparator(a: IHabit, b: IHabit): number {
+  return a.frequency - b.frequency;
+}
+
+export function lastIncrementedComparator(a: IHabit, b: IHabit): number {
+  if (a.lastIncrement == null) {
+    return -1;
+  }
+  if (b.lastIncrement == null) {
+    return 1;
+  }
+  return (
+    new Date(a.lastIncrement).getTime() - new Date(b.lastIncrement).getTime()
+  );
+}
--- client/app/dashboard/_ui/HabitGrid.tsx
+++ client/app/dashboard/_ui/HabitGrid.tsx
@@ -2,12 +2,31 @@
 import { useEffect, useState } from "react";
 import getHabits from "../_lib/getHabits";
 import Habit, { IHabit } from "./Habit";
+import {
+  nameComparator,
+  descriptionComparator,
+  streakComparator,
+  frequencyComparator,
+  lastIncrementedComparator,
+} from "../_lib/comparators";
 
 export default function HabitGrid({ sortedBy }: { sortedBy: number }) {
   const [habits, setHabits] = useState([] as IHabit[]);
+  const comparators = [
+    nameComparator,
+    descriptionComparator,
+    streakComparator,
+    frequencyComparator,
+    lastIncrementedComparator,
+  ];
+
   useEffect(() => {
     getHabits().then((data) => setHabits(data));
   }, []);
+
+  if (sortedBy != -1) {
+    habits.sort(comparators[sortedBy]);
+  }
   return (
     <div className="ml-2 grid basis-3/4 grid-cols-3 gap-3 overflow-y-scroll rounded-xl bg-grayscale-300 p-5">
       {habits.map((item: IHabit, index: number) => (
