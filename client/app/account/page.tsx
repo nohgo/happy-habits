fd266e5745da2ba3556c31304c633414c32b625b
nohgo <nohgoering@gmail.com>
2024-06-08 22:34:24 -0400

    add delete account box and delete navbar component

--- client/app/account/page.tsx
+++ client/app/account/page.tsx
@@ -1,5 +1,10 @@
+import DeleteAccountButton from "./_ui/DeleteAccountButton";
 import LogoutButton from "./_ui/LogoutButton";
 
 export default function Account({}) {
-  return <LogoutButton />;
+  return (
+    <>
+      <LogoutButton /> <DeleteAccountButton />
+    </>
+  );
 }
--- client/app/dashboard/page.tsx
+++ client/app/dashboard/page.tsx
@@ -1,6 +1,7 @@
 import Logo from "@/app/_ui/Logo";
 import Image from "next/image";
 import GridContainer from "./_ui/GridContainer";
+import Link from "next/link";
 
 export const metadata = {
   title: "Dashboard | Happy Habits",
@@ -12,7 +13,15 @@ export default function Dashboard() {
     <div className="flex h-screen flex-col justify-between bg-gradient-to-t from-grayscale-300 to-grayscale-50 px-10 pt-5 dark:from-grayscale-950 dark:to-grayscale-600">
       <div className="flex items-center justify-between">
         <Logo route="" />
-        <Image src="user-icon.svg" alt="User menu" width={50} height={50} />
+        <Link href={"/account"}>
+          {" "}
+          <Image
+            src="user-icon.svg"
+            alt="User menu"
+            width={50}
+            height={50}
+          />{" "}
+        </Link>
       </div>
       <div className="mb-10 mt-5 flex h-[85%] justify-between">
         <GridContainer />
